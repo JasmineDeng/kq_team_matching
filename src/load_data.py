d4279911faf5b4b7aaddbf6597c757e948d689bb
JasmineDeng <jasmine.deng@berkeley.edu>
2023-02-18 21:03:36 -0800

    clean up load data and add pre commit hooks

--- src/load_data.py
+++ src/load_data.py
@@ -1,15 +1,42 @@
 import csv
-from typing import Dict, Set
+from typing import Dict, Optional, Set
 
 from data_types import Player, PlayerRanking, PlayerRole
 
 
+def _try_to_int(value: str, default_value: int = 1) -> Optional[int]:
+    """Try to convert the provided value to an integer, if it fails, return the default value."""
+    try:
+        return int(value)
+    except Exception:
+        print(f"Could not convert {value} to integer")
+        return default_value
+
+
+def _load_all_rankings(data: Dict[str, str]) -> Dict[str, int]:
+    """Load all rankings for all roles given a dictionary representing a CSV row.
+
+    This function is currently not used.
+    """
+    ranking = {
+        role: data[key]
+        for role, key in [
+            (PlayerRole.QUEEN, "queen rank"),
+            (PlayerRole.VANILLA, "vanilla rank"),
+            (PlayerRole.SPEED, "speed rank"),
+            (PlayerRole.OBJECTIVE, "objective rank"),
+        ]
+    }
+    # convert the str to int
+    integer_ranking = {key: _try_to_int(val) for key, val in ranking.items()}
+    return integer_ranking
+
+
 def load_data(csv_path: str) -> Dict[str, PlayerRanking]:
-    players = set()
-    all_names = set()
+    all_names = []
 
     to_return = {}
-    with open(csv_path, newline='') as csvfile:
+    with open(csv_path, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             # Create rankings for the primary and secondary roles
@@ -19,32 +46,31 @@ def load_data(csv_path: str) -> Dict[str, PlayerRanking]:
             secondary_rank = int(row["secondary rank"])
 
             name = row["Name"]
+            to_return[name.lower().strip()] = PlayerRanking(
+                primary_role=primary_role,
+                primary_ranking=primary_rank,
+                secondary_ranking=secondary_rank,
+                secondary_role=secondary_role,
+            )
+            all_names.append(name)
 
-            # ranking = {role: row[key] for role, key in [(PlayerRole.QUEEN, "queen rank"), (PlayerRole.VANILLA, "vanilla rank"), (PlayerRole.SPEED, "speed rank"), (PlayerRole.OBJECTIVE, "objective rank")]}
-            # convert the str to int
-            # ranking = {key: _try_to_int(val) for key, val in ranking.items()}
-            to_return[name.lower().strip()] = PlayerRanking(primary_role=primary_role, primary_ranking=primary_rank,secondary_ranking=secondary_rank, secondary_role=secondary_role)
-            all_names.add(name)
-
-    # if len(all_names) != len(players):
-    #     raise ValueError(f"Got a duplicate player somewhere. Had {len(to_return)} players but {len(all_names)}")
+    if len(all_names) != len(to_return):
+        raise ValueError(
+            f"Got a duplicate player somewhere. Had {len(to_return)} players but {len(all_names)}"
+        )
     return to_return
 
 
-def load_attendance(csv_path: str, player_info: Dict[str, PlayerRanking]) -> Set[Player]:
+def load_attendance(
+    csv_path: str, player_info: Dict[str, PlayerRanking]
+) -> Set[Player]:
     players = set()
 
-    attendance_to_role_enum = {"Vanilla Warrior": PlayerRole.VANILLA, "Objective": PlayerRole.OBJECTIVE, "Speed Warrior": PlayerRole.SPEED, "Queen": PlayerRole.QUEEN}
-
-    with open(csv_path, newline='') as csvfile:
+    with open(csv_path, newline="") as csvfile:
         reader = csv.reader(csvfile)
-        next(reader) # skip the header
+        next(reader)  # skip the header
         for row in reader:
-            name, primary_role, secondary_role = row[1], row[2], row[3]
-            primary_role_enum = attendance_to_role_enum[primary_role]
-            secondary_role_enum = attendance_to_role_enum[secondary_role]
-
-            player_ranking = player_info[name.lower().strip()]
+            name = row[1]
             player = Player(name=name, ranking=player_info[name.lower().strip()])
             players.add(player)
     return players
