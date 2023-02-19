71bb2c8b31a884d64fd15c992699a00afc2c7737
JasmineDeng <jasmine.deng@berkeley.edu>
2023-02-18 21:23:02 -0800

    cleaning up and add click arg for the sampling strategy

--- src/main.py
+++ src/main.py
@@ -1,20 +1,39 @@
+import random
 
 import click
 
-from assignment import assign_players_to_teams
+from assignment import PlayerSamplingStrategy, assign_players_to_teams
 from find_fills import find_fills
 from load_data import load_attendance, load_data
 
 
+def _to_player_sampling_enum(_, __, value: str) -> PlayerSamplingStrategy:
+    return PlayerSamplingStrategy[value]
+
+
 @click.command()
-@click.option("--file-path", "-f", type=str, required=True, help="File path to csv file with player rankings.")
-def cli(file_path: str) -> None:
+@click.option(
+    "--file-path",
+    "-f",
+    type=str,
+    required=True,
+    help="File path to csv file with player rankings.",
+)
+@click.option(
+    "--player-sampling-strategy",
+    "-s",
+    type=click.Choice([e.name for e in PlayerSamplingStrategy]),
+    default=PlayerSamplingStrategy.PRIORITIZE_PREFERRED_ROLE.name,
+    callback=_to_player_sampling_enum,
+    help="The sampling strategy to use.",
+)
+def cli(file_path: str, player_sampling_strategy: PlayerSamplingStrategy) -> None:
 
     player_infos = load_data(file_path)
     all_players = load_attendance("data/attendance.csv", player_infos)
     print(all_players)
 
-    teams = assign_players_to_teams(all_players)
+    teams = assign_players_to_teams(all_players, player_sampling_strategy)
     teams = sorted(teams, key=lambda t: t.total_score)
     for t in teams:
         print(t)
@@ -34,15 +53,17 @@ def cli(file_path: str) -> None:
     if len(finalized_team_scores) > 0:
         finalized_score = sum(finalized_team_scores) / len(finalized_team_scores)
     else:
-        print("No finalized teams, we can use any fill so long as the players are roughly equal in skill")
+        print(
+            "No finalized teams, we can use any fill so long as the players are roughly equal in skill"
+        )
         finalized_score = sum(all_team_scores) / len(all_team_scores) + 2.5
 
     for t in teams:
         if t.needs_fill:
             possible_fills = find_fills(t, all_players, finalized_score)
             print(f"For team {t.team_name}, possible fills: {possible_fills}")
-            # subsample = random.sample(possible_fills, 2)
-            # print(f"Randomly chosen two fills: {subsample}")
+            subsample = random.sample(possible_fills, 2)
+            print(f"Randomly chosen two fills: {subsample}")
 
 
 if __name__ == "__main__":
