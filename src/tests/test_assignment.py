44eec449fdb30fa3056cda92d057e967740a309a
JoshuaBiedenweg <125229421+winningways2@users.noreply.github.com>
2023-02-25 15:54:31 -0800

    add exclusion set csv and implement assignment for exclusion (#7)
    Co-authored-by: JasmineDeng <jasmine.deng@berkeley.edu>

--- src/tests/test_assignment.py
+++ src/tests/test_assignment.py
@@ -1,71 +1,46 @@
-from src.assignment import _sample_players_by_highest_score, _sample_players_by_preferred_role
-from src.data_types import Player, PlayerRanking, PlayerRole
+from typing import Set
+
+from src.assignment import assign_players_to_teams
+from src.data_types import Player, PlayerRanking, PlayerRole, Team
+from src.sampling import PlayerSamplingStrategy
 
 
-def test_sample_players_by_highest_score() -> None:
-    players = [
-        Player(
-            name="A",
-            ranking=PlayerRanking(
-                primary_role=PlayerRole.VANILLA,
-                primary_ranking=3,
-                secondary_role=PlayerRole.OBJECTIVE,
-                secondary_ranking=4,
-            ),
-        ),
-        Player(
-            name="B",
-            ranking=PlayerRanking(
-                primary_role=PlayerRole.OBJECTIVE,
-                primary_ranking=2,
-                secondary_role=PlayerRole.VANILLA,
-                secondary_ranking=5,
-            ),
-        ),
-        Player(
-            name="C",
-            ranking=PlayerRanking(
-                primary_role=PlayerRole.OBJECTIVE,
-                primary_ranking=4,
-                secondary_role=PlayerRole.VANILLA,
-                secondary_ranking=5,
-            ),
-        ),
-    ]
-    assignments = _sample_players_by_highest_score(players, 1, PlayerRole.OBJECTIVE)
-    assert len(assignments) == 1
-    assert assignments[0].assigned_role == PlayerRole.OBJECTIVE
-    # Get C even if A and C have the same score, because C wants to play objective
-    assert assignments[0].player.name == "C"
-    assignments = _sample_players_by_highest_score(players, 2, PlayerRole.OBJECTIVE)
-    assert len(assignments) == 2
-    # Now we get A and C, B is ignored even if they want to do objective because the score is low
-    names = {a.player.name for a in assignments}
-    assert names == {"A", "C"}
+def _player_ranking_ignore_secondary(role: PlayerRole, ranking: int) -> PlayerRanking:
+    secondary_role = PlayerRole.VANILLA if role != PlayerRole.VANILLA else PlayerRole.OBJECTIVE
+    return PlayerRanking(primary_role=role, primary_ranking=ranking, secondary_role=secondary_role, secondary_ranking=1)
 
 
-def test_sample_players_by_preferred_role() -> None:
-    players = [
-        Player(
-            name="A",
-            ranking=PlayerRanking(
-                primary_role=PlayerRole.VANILLA,
-                primary_ranking=3,
-                secondary_role=PlayerRole.OBJECTIVE,
-                secondary_ranking=4,
-            ),
-        ),
-        Player(
-            name="B",
-            ranking=PlayerRanking(
-                primary_role=PlayerRole.OBJECTIVE,
-                primary_ranking=2,
-                secondary_role=PlayerRole.VANILLA,
-                secondary_ranking=5,
-            ),
-        ),
-    ]
-    assignments = _sample_players_by_preferred_role(players, 1, PlayerRole.OBJECTIVE)
-    assert len(assignments) == 1
-    assert assignments[0].assigned_role == PlayerRole.OBJECTIVE
-    assert assignments[0].player.name == "B"
+def _team_to_player_names(team: Team) -> Set[str]:
+    return {p.player.name for p in team.players}
+
+
+def test_exclusion_assign_players_to_teams() -> None:
+    all_players = {
+        # Team 1
+        Player(name="A", ranking=_player_ranking_ignore_secondary(PlayerRole.QUEEN, 5)),
+        Player(name="B", ranking=_player_ranking_ignore_secondary(PlayerRole.SPEED, 5)),
+        Player(name="C", ranking=_player_ranking_ignore_secondary(PlayerRole.OBJECTIVE, 5)),
+        Player(name="D", ranking=_player_ranking_ignore_secondary(PlayerRole.VANILLA, 5)),
+        # Team 2
+        Player(name="E", ranking=_player_ranking_ignore_secondary(PlayerRole.QUEEN, 4)),
+        Player(name="F", ranking=_player_ranking_ignore_secondary(PlayerRole.SPEED, 4)),
+        Player(name="G", ranking=_player_ranking_ignore_secondary(PlayerRole.VANILLA, 5)),
+        Player(name="H", ranking=_player_ranking_ignore_secondary(PlayerRole.OBJECTIVE, 5)),
+    }
+    teams = assign_players_to_teams(all_players, PlayerSamplingStrategy.PRIORITIZE_HIGHEST_SCORE, [])
+    assert len(teams) == 2
+    # Sort by team name (queen name)
+    teams.sort(key=lambda team: team.team_name)
+    team_player_names = _team_to_player_names(teams[0])
+    assert "A" in team_player_names and "F" in team_player_names
+    team_player_names = _team_to_player_names(teams[1])
+    assert "E" in team_player_names and "B" in team_player_names
+
+    teams = assign_players_to_teams(all_players, PlayerSamplingStrategy.PRIORITIZE_HIGHEST_SCORE, [{"A", "F"}])
+    assert len(teams) == 2
+    # Sort by team name (queen name)
+    teams.sort(key=lambda team: team.team_name)
+    team_player_names = _team_to_player_names(teams[0])
+    assert "A" in team_player_names and "B" in team_player_names
+    team_player_names = _team_to_player_names(teams[1])
+    assert "E" in team_player_names and "F" in team_player_names
---
+++ src/tests/test_sampling.py
@@ -0,0 +1,71 @@
+from src.data_types import Player, PlayerRanking, PlayerRole
+from src.sampling import _sample_players_by_highest_score, _sample_players_by_preferred_role
+
+
+def test_sample_players_by_highest_score() -> None:
+    players = [
+        Player(
+            name="A",
+            ranking=PlayerRanking(
+                primary_role=PlayerRole.VANILLA,
+                primary_ranking=3,
+                secondary_role=PlayerRole.OBJECTIVE,
+                secondary_ranking=4,
+            ),
+        ),
+        Player(
+            name="B",
+            ranking=PlayerRanking(
+                primary_role=PlayerRole.OBJECTIVE,
+                primary_ranking=2,
+                secondary_role=PlayerRole.VANILLA,
+                secondary_ranking=5,
+            ),
+        ),
+        Player(
+            name="C",
+            ranking=PlayerRanking(
+                primary_role=PlayerRole.OBJECTIVE,
+                primary_ranking=4,
+                secondary_role=PlayerRole.VANILLA,
+                secondary_ranking=5,
+            ),
+        ),
+    ]
+    assignments = _sample_players_by_highest_score(players, 1, PlayerRole.OBJECTIVE)
+    assert len(assignments) == 1
+    assert assignments[0].assigned_role == PlayerRole.OBJECTIVE
+    # Get C even if A and C have the same score, because C wants to play objective
+    assert assignments[0].player.name == "C"
+    assignments = _sample_players_by_highest_score(players, 2, PlayerRole.OBJECTIVE)
+    assert len(assignments) == 2
+    # Now we get A and C, B is ignored even if they want to do objective because the score is low
+    names = {a.player.name for a in assignments}
+    assert names == {"A", "C"}
+
+
+def test_sample_players_by_preferred_role() -> None:
+    players = [
+        Player(
+            name="A",
+            ranking=PlayerRanking(
+                primary_role=PlayerRole.VANILLA,
+                primary_ranking=3,
+                secondary_role=PlayerRole.OBJECTIVE,
+                secondary_ranking=4,
+            ),
+        ),
+        Player(
+            name="B",
+            ranking=PlayerRanking(
+                primary_role=PlayerRole.OBJECTIVE,
+                primary_ranking=2,
+                secondary_role=PlayerRole.VANILLA,
+                secondary_ranking=5,
+            ),
+        ),
+    ]
+    assignments = _sample_players_by_preferred_role(players, 1, PlayerRole.OBJECTIVE)
+    assert len(assignments) == 1
+    assert assignments[0].assigned_role == PlayerRole.OBJECTIVE
+    assert assignments[0].player.name == "B"
