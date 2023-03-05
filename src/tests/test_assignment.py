470c04e9f00f9a9e9ed56264987fedfd091a139f
Jasmine Deng <jasmine.deng@berkeley.edu>
2023-03-05 15:23:44 -0800

    improve missing roles error message, add some tests, add warning messages (#9)

--- src/tests/test_assignment.py
+++ src/tests/test_assignment.py
@@ -1,5 +1,7 @@
 from typing import Set
 
+import pytest
+
 from src.assignment import assign_players_to_teams
 from src.data_types import Player, PlayerRole, Team
 
@@ -44,3 +46,26 @@ def test_exclusion_assign_players_to_teams() -> None:
     assert "A" in team_player_names and "B" in team_player_names
     team_player_names = _team_to_player_names(teams[1])
     assert "E" in team_player_names and "F" in team_player_names
+
+
+def test_allows_flex_fills() -> None:
+    # In a sad day, we only have 6 people (2 queen, 2 speed, 2 obj), but theoretically we should still allow any
+    # flex position to fill it.
+    all_players = {
+        # Team 1
+        _player_one_role("A", PlayerRole.QUEEN, 10),
+        _player_one_role("B", PlayerRole.SPEED, 10),
+        _player_one_role("C", PlayerRole.OBJECTIVE, 10),
+        _player_one_role("D", PlayerRole.FLEX, 10),
+        # Team 2
+        _player_one_role("E", PlayerRole.QUEEN, 8),
+        _player_one_role("F", PlayerRole.OBJECTIVE, 8),
+    }
+    with pytest.raises(ValueError):
+        assign_players_to_teams(all_players, [])
+
+    all_players.add(
+        _player_one_role("H", PlayerRole.SPEED, 8),
+    )
+    teams = assign_players_to_teams(all_players, [])
+    assert len(teams) == 2
